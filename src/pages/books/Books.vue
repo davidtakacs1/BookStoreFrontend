794de0bc427ffb2d09ebd120af1f4cca39e2d6cf
takacs.david <takacs.david@C202-13>
2023-04-17 09:15:03 +0200

    change

--- src/pages/books/Books.vue
+++ src/pages/books/Books.vue
@@ -7,42 +7,5 @@ import Footer from '../../components/Footer.vue';
 <style scoped src="./books.css"></style>
 
 <script>
-import { mapState, mapActions } from 'pinia'
 
-export default {
-  components: {
-    TopNavbar,
-    Footer
-  },
-
-  mounted() {
-    //console.log('mounted', this.contents)
-  },
-
-  computed: {
-    ...mapState(useCart, {
-      contents: 'getContents',
-      count: 'count',
-      cartTotal: 'total'
-    }),
-
-    total() {
-      return Math.max(this.cartTotal - this.promo.value, 0).toFixed(2);
-    }
-  },
-
-  data() {
-    return {
-      currency: {
-        name: 'USD',
-        symbol: '$'
-      },
-
-      promo: {
-        code: 'EXAMPLECODE',
-        value: 5
-      }
-    };
-  },
-};
 </script>
