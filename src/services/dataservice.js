c1065e65ff050be21b0c59b557714d08b7b4af4e
Németh Dávid <nemeth.david@students.jedlik.eu>
2023-04-18 12:16:37 +0200

    még több Axios függvény

--- src/services/dataservice.js
+++ src/services/dataservice.js
@@ -6,6 +6,34 @@ export default {
         return Axios.get(`/products/${productId}`);
     },
     getProductArray(startingNum){
-        return Axios.get(`/products/arr/${startiNgnum}`)
+        return Axios.get(`/products/arr/${startingNum}`);
+    },
+    createOrder(newOrder){
+        return Axios.post('/orders',{
+            userId: newOrder.userId,
+            paymentStatus: newOrder.paymentStatus,
+            status: newOrder.status,
+            billingAddress: newOrder.billingAddress
+        });
+    },
+    createOrderDetail(newOrderDetail){
+        return Axios.post('/orderdetails',{
+            productId: newOrderDetail.productId,
+            orderId: newOrderDetail.orderId,
+            archived_price: newOrderDetail.archived_price,
+            quantity:newOrderDetail.quantity
+        });
+    },
+    modifyUser(user){
+        return Axios.patch('/users',{user});
+    },
+    registration(user){
+        return Axios.post('/auth/register',{user});
+    },
+    login(user){
+        return Axios.post('/auth/login',{user});
+    },
+    logout(){
+        return Axios.post('/auth/logout');
     }
 }
