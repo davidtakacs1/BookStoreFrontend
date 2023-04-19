ccad028b008962b07025e947f916f5c1c3ccad25
Németh Dávid <nemeth.david@students.jedlik.eu>
2023-04-19 11:31:38 +0200

    regisztráció alatt adatok átmeneti tárolása

--- src/pages/user/register/Register.vue
+++ src/pages/user/register/Register.vue
@@ -4,6 +4,29 @@ import Footer from '../../../components/Footer.vue';
 import { ref } from 'vue';
 
 const billing=ref(false);
+const newuser=ref({
+  firstName:"",
+  lastName:"",
+  email:"",
+  email_verified:false,
+  auto_login:false,
+  password:"",
+  roles:[
+    "user"
+  ],
+  address:{
+    street:"",
+    city:"",
+    country:"",
+    zip:""
+  },
+  shippingAddress:{
+    street:"",
+    city:"",
+    country:"",
+    zip:""
+  }
+})
 </script>
 
 <template src="./register.html"></template>
