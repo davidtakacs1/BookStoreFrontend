a7241c92958755a0641ff87277a8affbc0dc612c
Németh Dávid <nemeth.david@students.jedlik.eu>
2023-04-19 11:01:03 +0200

    changes on register, only transitionary while prepping for scripting it

--- src/pages/user/register/Register.vue
+++ src/pages/user/register/Register.vue
@@ -1,7 +1,9 @@
 <script setup>
 import TopNavbar from '../../../components/TopNavbar.vue';
 import Footer from '../../../components/Footer.vue';
+import { ref } from 'vue';
 
+const billing=ref(false);
 </script>
 
 <template src="./register.html"></template>
