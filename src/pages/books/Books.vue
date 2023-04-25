1837f0d5f55dd49454b0218ebeee63b4f344c398
davidtakacs1 <takacs.david@students.jedlik.eu>
2023-04-25 22:05:40 +0200

    netlify

--- src/pages/books/Books.vue
+++ src/pages/books/Books.vue
@@ -1,7 +1,7 @@
 <script setup>
 import TopNavbar from '../../components/TopNavbar.vue';
 import Footer from '../../components/Footer.vue';
-// import dataservice from '../../services/dataservice';
+import dataservice from '../../services/dataservice';
 import { ref } from 'vue';
 
 const products = ref([])
@@ -14,6 +14,7 @@ dataservice.getProducts()
     .catch((err) => {
         console.log(err);
     });
+    
 </script>
 
 <template>
